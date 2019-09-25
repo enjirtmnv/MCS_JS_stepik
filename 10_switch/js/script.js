afbac9a308707e8503821a39ba766dcf814cf43c
enjirtmnv <shkensinio@gmail.com>
2019-09-25 21:45:15 +0300

    switch

---
+++ 10_switch/js/script.js
@@ -0,0 +1,16 @@
+let answer = prompt('Enter the name of the musician');
+answer = answer.toLowerCase()
+
+switch (answer) {
+    case 'dr. dre':
+        console.log('rap');
+        break;
+    case 'jimmy hendrix':
+        console.log('rock');
+        break;
+    case  'britney spears':
+        console.log('pop');
+        break;
+    default:
+        console.log('no such singer in database');
+}
