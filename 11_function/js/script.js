538f557900b5ba96f5cacc9814f04e8030c45b4e
enjirtmnv <shkensinio@gmail.com>
2019-09-26 11:49:10 +0300

    function

---
+++ 11_function/js/script.js
@@ -0,0 +1,12 @@
+function sayHi() {
+    return 'Hello, student';
+}
+console.log(sayHi());
+alert(sayHi());
+document.body.innerHTML = sayHi();
+
+function sayName(name) {
+    return 'Hello, ' + name;
+}
+console.log(sayName('Enji'));
+console.log(sayName('mom'));
