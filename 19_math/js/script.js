e93874cafc8a2f1a642b26fe286ebf1c6e16dee2
enjirtmnv <shkensinio@gmail.com>
2019-10-04 19:42:24 +0300

    math

---
+++ 19_math/js/script.js
@@ -0,0 +1,13 @@
+function modulSquare(num) {
+    return Math.abs(num)/2
+}
+
+console.log(modulSquare(-3));
+console.log(modulSquare(3));
+
+console.log(Math.floor(2.456));
+console.log(Math.round(2.789));
+
+console.log(Math.random());
+
+console.log(Math.floor((Math.random()*(10-5)+5)));
