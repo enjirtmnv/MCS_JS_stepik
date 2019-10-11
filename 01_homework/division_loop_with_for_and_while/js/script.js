62807fcb76caa7b38c8a905a6d73057464baca23
enjirtmnv <shkensinio@gmail.com>
2019-10-11 19:58:47 +0300

    division loop with for and while

---
+++ 01_homework/division_loop_with_for_and_while/js/script.js
@@ -0,0 +1,21 @@
+//while loop
+
+let i = 1;
+while (i <= 100){
+    if (i % 6 == 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+
+
+
+//for classic loop
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 6 == 0) {
+        console.log(i);
+    }
+};
+
