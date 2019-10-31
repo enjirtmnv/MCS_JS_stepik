d5b8b7eb2d7e5f1054a14dc954b2f6cc7dc02cd0
enjirtmnv <shkensinio@gmail.com>
2019-11-01 00:12:15 +0300

    done final task

--- 01_homework/FINAL_TASK/js/script.js
+++ 01_homework/FINAL_TASK/js/script.js
@@ -1,26 +1,13 @@
 window.addEventListener('DOMContentLoaded',function () {
-    const APIKey = 'bf7c6e155eea7fd1ee828f0134cd6aaa';
-    const city = 'Moscow';
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-    let xhr = new XMLHttpRequest;
-    xhr.open('GET', url, true);
-    xhr.send();
-
-    if (xhr.status != 200){
-        console.log(xhr.status + ' ' +xhr.statusText);
-    } else {
-        let DATA = JSON.parse(xhr.response);
-        let temp = (DATA.main.temp - 273).toFixed(1);
-        console.log(temp);
-    }
 
     class Person{
-        constructor(name){
+        constructor(name, temp){
             this.name = name;
             this.happiness = 0;
+            this.temp = temp;
         }
 
-        hasCat() {
+        hasLove() {
             return this.happiness += 1;
         }
 
@@ -33,17 +20,86 @@ window.addEventListener('DOMContentLoaded',function () {
         }
 
         isSunny() {
-            if (temp > 15){
+            if (this.temp > 15){
                 return this.happiness += 1;
             } else {
-                return this.happiness
+                return this.happiness;
+            }
+        }
+        get() {
+            console.log(this.happiness)
+        }
+
+        totalCheck(){
+            const icon = document.querySelector('.icon');
+            if (this.happiness == 4){
+                icon.innerHTML = '😍️';
+            } else if (this.happiness == 3 || this.happiness == 2){
+                icon.innerHTML = '☺️';
+            } else {
+                icon.innerHTML = '😭';
             }
         }
     }
 
-    const form = document.forms[0];
-    console.log(form);
-    form.onsubmit = function (e) {
-        e.preventDefault();
+    const APIKey = 'bf7c6e155eea7fd1ee828f0134cd6aaa';
+    const city = 'Sydney';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', url, false);
+    xhr.send();
+
+    let tempData = 0;
+    if (xhr.status != 200){
+        console.log(xhr.status + ' ' + xhr.statusText);
+    } else {
+        var DATA = JSON.parse(xhr.response);
+        console.log(DATA);
+        tempData = (DATA.main.temp - 273).toFixed(1);  //var виден снаружи if для экземпляра person
+        console.log(tempData);
     }
+
+    const form = document.forms[0];
+
+
+        console.log(form);
+
+
+        form.onsubmit = function (e) {
+            e.preventDefault();
+
+            const nameEntered = form.elements.name.value;
+            const love = form.elements.love.value;
+            const rest = form.elements.rest.value;
+            const money = form.elements.money.value;
+
+
+            let boo = new Person(nameEntered, tempData);
+
+            if (love.toLowerCase() === 'yes'){
+                boo.hasLove();
+            }
+
+            if (rest.toLowerCase() === 'yes'){
+                boo.hasRest();
+            }
+
+            if (money.toLowerCase() === 'yes'){
+                boo.hasMoney();
+            }
+
+            boo.isSunny();
+            boo.get();
+
+            const personName = document.querySelector('.personName');
+
+            personName.innerHTML = nameEntered;
+
+            boo.totalCheck()
+
+        }
+
+
 });
+
+
